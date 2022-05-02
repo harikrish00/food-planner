1bfca93090832d024e73425b36260eb44179deaa
Harikrishnan Narasiman <harikrish@scadoosh.com>
2022-05-01 23:33:34 -0400

    first version

--- handler.py
+++ handler.py
@@ -3,20 +3,29 @@ import random
 
 
 def pick_random_side_dish(side_dish_list: list) -> str:
-    return random.choice(side_dish_list)
+    return random_choice(side_dish_list)
+
+
+def random_choice(item_list):
+    return random.choice(item_list)
 
 
 def food_schedule_of_the_week(food_items: dict):
     NUM_OF_DAYS = 5
     main_dish_list: list = food_items['mainDish']
-    vegetables_needed = []
+    side_dishes: dict = food_items['sideDish']
     for i in range(NUM_OF_DAYS):
+        ingredients_needed: list = []
         len_of_main_dish_list: int = len(main_dish_list)
-        chosen_main_dish: dict = random.randint(0,len_of_main_dish_list-1)
-        print(main_dish_list[chosen_main_dish])
-        side_dish: str = pick_random_side_dish(main_dish_list[chosen_main_dish]["sideDish"])
+        chosen_main_dish_index: int = random.randint(0,len_of_main_dish_list-1)
+        chosen_main_dish = main_dish_list[chosen_main_dish_index]
+        ingredients_needed.append(random.choice(chosen_main_dish["ingredientsNeeded"]))
+        side_dish: str = pick_random_side_dish(chosen_main_dish["sideDish"])
+        ingredients_needed += side_dishes[side_dish]["ingredientsNeeded"]
+        print(chosen_main_dish)
         print(f'side dish:{side_dish}')
-        main_dish_list.pop(chosen_main_dish)
+        print(set(ingredients_needed))
+        main_dish_list.pop(chosen_main_dish_index)
         
     #number of days
     #for each day pick a random main dish
